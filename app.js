6eaf8c0714c56f8f8eb712349500e10877e7e1ae
gratisreise <104671487+gratisreise@users.noreply.github.com>
2022-05-02 21:21:25 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + " nico");
+
+myName = "nicolas";
+
+console.log("your new name is" + myName);
